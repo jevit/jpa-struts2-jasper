e5db56fda204827bcf7ac1ffd8d4a24f848122fc
jevit <jeremie.viteau@gmail.com>
2014-10-14 18:37:55 +0200

    validation

--- struts2-jpa-spring/src/main/java/struts2/action/UtilisateurAction.java
+++ struts2-jpa-spring/src/main/java/struts2/action/UtilisateurAction.java
@@ -14,10 +14,37 @@ public class UtilisateurAction extends ActionSupport {
 	public static final String MESSAGE = "hello";
 
 	private String message;
+	private String username;
+	private String password;
 
 	public String execute() throws Exception {
 
 		return SUCCESS;
+	}
+
+	// simple validation
+	public void validate() {
+		if ("admin".equals(getUsername())) {
+			addActionMessage("You are valid user!");
+		} else {
+			addActionError("Invalide login");
+		}
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getMessage() {
